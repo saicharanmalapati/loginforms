a11daffb92a7122b2c0046dc9dc0e6397b6f6cc7
eswar2106 <eswarmavuru1607@gmail.com>
2018-08-27 12:51:49 +0530

    pagination

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-from flask import Flask, render_template, request, flash
-from forms import LoginForm
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.secret_key = 'development key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["TEMPLATE_AUTO_RELOAD"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SECRET_KEY'] = "random"
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+class students(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    city = db.Column(db.String(20))
+    addr = db.Column(db.String(30))
+    pin = db.Column(db.String(10))
+
+    def __init__(self, name, city, addr, pin):
+        self.name = name
+        self.city = city
+        self.addr = addr
+        self.pin = pin
 
 
-@app.route('/signin', methods=['POST'])
-def signin():
-    # form = request.LoginForm()
+@app.route('/')
+def show_all1():
+    page = request.args.get('page', 1, type=int)
+    stds = students.query.paginate(page=page, per_page=2)
+    return render_template('show_all1.html', students=stds)
 
-    # import ipdb
-    # ipdb.set_trace()
 
-    email = request.form["email"]
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+            flash('Please enter all the fields', 'error')
+        else:
+            student = students(request.form['name'], request.form['city'],
+                               request.form['addr'], request.form['pin'])
 
-    return render_template('user.html', email=email)
+            db.session.add(student)
+            db.session.commit()
+            flash('Record was successfully added')
+            return redirect(url_for('show_all1'))
+    return render_template('new.html')
 
 if __name__ == '__main__':
-    app.run()
+    db.create_all()
+    app.run(debug=True)
